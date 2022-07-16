9302210cf1ede27f9412a3aebcd7d73749ce5715
Bruno Barbosa <brb.dev@outlook.com>
2022-07-16 17:59:22 -0400

    inicia modelagem de entidades

--- banco/models.py
+++ banco/models.py
@@ -1,3 +1,19 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+	id = models.AutoField(primary_key=True)
+	nome = models.CharField(max_length=50)
+	sobrenome = models.CharField(max_length=150)
+	dataNascimento = models.DateField
+	cpf = models.CharField(max_length=11)
+	email = models.CharField(max_length=255)
+
+class Conta(models.Model):
+	id = models.AutoField(primary_key=True)
+	agencia = models.CharField(max_length=4)
+	numero = models.CharField(max_length=10)
+	saldo = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+	cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+
