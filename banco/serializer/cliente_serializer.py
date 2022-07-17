764801d8aadfabe2ef8bf95b81a3e65fefd421c8
Bruno Barbosa <brb.dev@outlook.com>
2022-07-17 02:08:01 -0400

    validacoes de cpf e email

--- banco/serializer/cliente_serializer.py
+++ banco/serializer/cliente_serializer.py
@@ -1,9 +1,26 @@
+import re
 from rest_framework import serializers
 
 from banco.model.cliente import Cliente
 
 class ClienteSerializer(serializers.ModelSerializer):
+
+	cpf = serializers.CharField(min_length=11)
 	
 	class Meta():
 		model = Cliente
 		fields = "__all__"
+
+	def validate_cpf(self, cpf):
+		if not re.match(r'^([\s\d]+)$', cpf):
+			raise serializers.ValidationError(
+				{'CPF': 'CPF deve ser numérico'}
+			)
+		return cpf
+
+	def validate_email(self, email):
+		if not re.match(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email):
+			raise serializers.ValidationError(
+				{'email': 'E-mail inválido'}
+			)
+		return email
