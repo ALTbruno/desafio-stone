fd36df499b0b566e2da666af7ad90218a4c333d8
Bruno Barbosa <brb.dev@outlook.com>
2022-07-16 20:12:59 -0400

    configura djangorestframework

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'banco.apps.BancoConfig'
 ]
 
