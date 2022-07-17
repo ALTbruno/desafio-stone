986b1c492997382883ceaa0e2de0b5f38a216fa1
Bruno Barbosa <brb.dev@outlook.com>
2022-07-17 15:56:59 -0400

    modela transacoes

---
+++ banco/model/transacao.py
@@ -0,0 +1,27 @@
+from tkinter import CASCADE
+from django.db import models
+
+from banco.model.conta import Conta
+
+class Transacao(models.Model):
+
+	DEPOSITO = 'DPST'
+	SAQUE = 'SQUE'
+	TRANSFERENCIA_RECEBIDA = 'TFRE'
+	TRANSFERENCIA_ENVIADA = 'TFEN'
+
+	TRANSACAO_CHOICES = [
+        (DEPOSITO, 'Deposito'),
+        (SAQUE, 'Saque'),
+		(TRANSFERENCIA_RECEBIDA, 'Transferencia_Recebida'),
+		(TRANSFERENCIA_ENVIADA, 'Transferencia_Enviada')
+    ]
+
+	id = models.AutoField(primary_key=True)
+	data_hora = models.DateTimeField(null=True)
+	valor = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+	tipo = models.CharField(max_length=5, choices=TRANSACAO_CHOICES)
+	conta = models.ForeignKey(Conta, on_delete=models.CASCADE)
+
+	def __str__(self) -> str:
+		return self.id
